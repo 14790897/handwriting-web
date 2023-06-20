4527c6748e40e3f9b17ea5fd3fe56cff891942cb
14790897 <14790897abc@gmail.com>
2023-06-20 14:23:10 +0800

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -30,80 +30,92 @@ app.config['MAX_CONTENT_LENGTH'] = 128 * 1024 * 1024
 
 @app.route('/api/generate_handwriting', methods=['POST'])
 def generate_handwriting():
-    data = request.form
-    print('request.form', data)
-    print('request.files', request.files)
-    text_to_generate = data['text']
-    if data['preview'] == 'true':
-        #截短字符，只生成一面
-        preview_length = 400  # 可以调整为所需的预览长度
-        text_to_generate = text_to_generate[:preview_length]
-    background_image = request.files['background_image'].read()
-    background_image = Image.open(io.BytesIO(background_image))
-    font = request.files['font_path'].read()
-    font = ImageFont.truetype(io.BytesIO(font), size=int(data['font_size']))
+    try:
+        data = request.form
+        required_fields = ['text', 'font_size', 'line_spacing', 'fill', 'left_margin', 'top_margin', 
+                           'right_margin', 'bottom_margin', 'word_spacing', 'line_spacing_sigma', 
+                           'font_size_sigma', 'word_spacing_sigma', 'perturb_x_sigma', 'perturb_y_sigma', 
+                           'perturb_theta_sigma']
+        for field in required_fields:
+            if field not in data:
+                return jsonify({'status': 'fail', 'message': f'Missing required field: {field}'}), 400
 
-    template = Template(
-        background=background_image,
-        font=font,
-        line_spacing=int(data['line_spacing']) + int(data['font_size']),
-        fill = ast.literal_eval(data['fill'])[:3],  # Ignore the alpha value
-        # fill=(int(data['red']), int(data['green']), int(data['blue'])),  # 字体“颜色”
-        left_margin=int(data['left_margin']),
-        top_margin=int(data['top_margin']),
-        right_margin=int(data['right_margin']) - int(data['word_spacing']) * 2,
-        bottom_margin=int(data['bottom_margin']),
-        word_spacing=int(data['word_spacing']),
-        line_spacing_sigma=int(data['line_spacing_sigma']),  # 行间距随机扰动
-        font_size_sigma=int(data['font_size_sigma']),  # 字体大小随机扰动
-        word_spacing_sigma=int(data['word_spacing_sigma']),  # 字间距随机扰动
-        end_chars="，。",  # 防止特定字符因排版算法的自动换行而出现在行首
-        perturb_x_sigma=int(data['perturb_x_sigma']),  # 笔画横向偏移随机扰动
-        perturb_y_sigma=int(data['perturb_y_sigma']),  # 笔画纵向偏移随机扰动
-        perturb_theta_sigma=float(data['perturb_theta_sigma']),  # 笔画旋转偏移随机扰动
-    )
-    images = handwrite(text_to_generate, template)
-    cursor = g.cnx.cursor()
+        data = request.form
+        print('request.form', data)
+        print('request.files', request.files)
+        text_to_generate = data['text']
+        if data['preview'] == 'true':
+            #截短字符，只生成一面
+            preview_length = 400  # 可以调整为所需的预览长度
+            text_to_generate = text_to_generate[:preview_length]
+        background_image = request.files['background_image'].read()
+        background_image = Image.open(io.BytesIO(background_image))
+        font = request.files['font_path'].read()
+        font = ImageFont.truetype(io.BytesIO(font), size=int(data['font_size']))
 
-    # 创建一个BytesIO对象，用于保存.zip文件的内容
-    zip_io = io.BytesIO()
-    with zipfile.ZipFile(zip_io, 'w') as zipf:
-        # 遍历生成的所有图片
-        for i, im in enumerate(images):
-            im.save("./output/{}.png".format(i))
-            # 将每张图片保存为一个BytesIO对象
-            img_io = io.BytesIO()
-            im.save(img_io, 'PNG')
-            img_io.seek(0)
-            if not data['preview']:
-                # 将图片BytesIO对象添加到.zip文件中
-                zipf.writestr(f"{i}.png", img_io.getvalue())
+        template = Template(
+            background=background_image,
+            font=font,
+            line_spacing=int(data['line_spacing']) + int(data['font_size']),
+            fill = ast.literal_eval(data['fill'])[:3],  # Ignore the alpha value
+            # fill=(int(data['red']), int(data['green']), int(data['blue'])),  # 字体“颜色”
+            left_margin=int(data['left_margin']),
+            top_margin=int(data['top_margin']),
+            right_margin=int(data['right_margin']) - int(data['word_spacing']) * 2,
+            bottom_margin=int(data['bottom_margin']),
+            word_spacing=int(data['word_spacing']),
+            line_spacing_sigma=int(data['line_spacing_sigma']),  # 行间距随机扰动
+            font_size_sigma=int(data['font_size_sigma']),  # 字体大小随机扰动
+            word_spacing_sigma=int(data['word_spacing_sigma']),  # 字间距随机扰动
+            end_chars="，。",  # 防止特定字符因排版算法的自动换行而出现在行首
+            perturb_x_sigma=int(data['perturb_x_sigma']),  # 笔画横向偏移随机扰动
+            perturb_y_sigma=int(data['perturb_y_sigma']),  # 笔画纵向偏移随机扰动
+            perturb_theta_sigma=float(data['perturb_theta_sigma']),  # 笔画旋转偏移随机扰动
+        )
+        images = handwrite(text_to_generate, template)
+        cursor = g.cnx.cursor()
 
-            # 保存图片到数据库
-            username = session['username']
-            sql = f"INSERT INTO user_images (username, image) VALUES (%s, %s)"
-            image_data = img_io.getvalue()
-            params = (username, image_data)
-            try:
-                # 执行 SQL 语句
-                cursor.execute(sql, params)
-                # 提交到数据库执行
-                g.cnx.commit()
-            except:
-                # 发生错误时回滚
-                g.cnx.rollback()
+        # 创建一个BytesIO对象，用于保存.zip文件的内容
+        zip_io = io.BytesIO()
+        with zipfile.ZipFile(zip_io, 'w') as zipf:
+            # 遍历生成的所有图片
+            for i, im in enumerate(images):
+                im.save("./output/{}.png".format(i))
+                # 将每张图片保存为一个BytesIO对象
+                img_io = io.BytesIO()
+                im.save(img_io, 'PNG')
+                img_io.seek(0)
+                if not data['preview']:
+                    # 将图片BytesIO对象添加到.zip文件中
+                    zipf.writestr(f"{i}.png", img_io.getvalue())
 
-            if data['preview']:
-                return send_file(io.BytesIO(image_data), mimetype='image/png')
+                # 保存图片到数据库
+                username = session['username']
+                sql = f"INSERT INTO user_images (username, image) VALUES (%s, %s)"
+                image_data = img_io.getvalue()
+                params = (username, image_data)
+                try:
+                    # 执行 SQL 语句
+                    cursor.execute(sql, params)
+                    # 提交到数据库执行
+                    g.cnx.commit()
+                except:
+                    # 发生错误时回滚
+                    g.cnx.rollback()
 
-    # 将BytesIO对象的位置重置到开始
-    zip_io.seek(0)
-    if not data['preview']:
-        # 返回.zip文件
-        return send_file(zip_io,
-                         attachment_filename='images.zip',
-                         mimetype='application/zip',
-                         as_attachment=True)
+                if data['preview']:
+                    return send_file(io.BytesIO(image_data), mimetype='image/png')
+
+        # 将BytesIO对象的位置重置到开始
+        zip_io.seek(0)
+        if not data['preview']:
+            # 返回.zip文件
+            return send_file(zip_io,
+                            attachment_filename='images.zip',
+                            mimetype='application/zip',
+                            as_attachment=True)
+    except Exception as e:
+        return jsonify({'status': 'error', 'message': str(e)}), 500
 
 
 @app.route('/api/login', methods=['POST'])
