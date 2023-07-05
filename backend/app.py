4345e42cfb99786c65e88cf30d75ef37e95b769b
Liuweiqing <121866954+14790897@users.noreply.github.com>
2023-07-05 08:47:25 +0800

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -197,6 +197,7 @@ def generate_handwriting():
     else:
         font_option = data['font_option']
         logger.info(f"font_option: {font_option}")
+        logger.info(f"font_file_names: {font_file_names}")
         if font_option in font_file_names:
             # 确定字体文件的完整路径
             font_path = os.path.join('font_assets', font_option)
